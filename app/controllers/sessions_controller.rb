f419bd1e9233be39c58cb52131bd7bc85a686745
John K. Towery <kbaries@gmail.com>
2016-08-26 14:16:40 -0600

    Added code to list out hash's content

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,10 @@ class SessionsController < ApplicationController
 		user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
 		session[:user_id] = user.id
 		@auth_hash = auth
+		@info_hash = auth["info"]
+		@cred_hash = auth["credentials"]
+		@extra_hash = auth["extra"]
+		@raw_hash = auth["extra"]["raw_info"]
 		# redirect_to root_url, :notice => "Signed in!"
 	end
 
